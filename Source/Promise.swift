2f66cab9994ac0f214b877e762f99e74281a219d
jake <jake@geocaching.com>
2017-10-10 11:02:29 -0700

    childPromiseLinkedList:  Implemented a linked list-like functionality, using optional child promises. - This allows multiple then blocks to be executed by each additional one past the first being put into a child promise and appended to the end of the list. - Also added explicit access-level notation to all methods and properties.

--- Source/Promise.swift
+++ Source/Promise.swift
@@ -26,6 +26,7 @@ class Future<T> {
     
     private var thenBlock: ThenBlock?
     private var errorBlock: ErrorBlock?
+    private var childFuture: Future?
     
     private let operationQueue = OperationQueue()
     
@@ -34,39 +35,51 @@ class Future<T> {
     private(set) var value: T?
     private(set) var error: Error?
     
-    var succeeded: Bool {
+    public var succeeded: Bool {
         return value != nil
     }
     
-    var failed: Bool {
+    public var failed: Bool {
         return error != nil
     }
     
-    var isComplete: Bool {
+    public var isComplete: Bool {
         return succeeded || failed
     }
     
-    @discardableResult func then(_ callback: @escaping ThenBlock) -> Future<T> {
+    @discardableResult public func then(_ callback: @escaping ThenBlock) -> Future<T> {
         operationQueue.addOperation {
             if let value = self.value { //If the future has already been resolved with a value. Call the block immediately.
                 callback(value)
             }
             
-            self.thenBlock = callback
+            if self.thenBlock == nil {
+                self.thenBlock = callback
+            }
+            else {
+                self.appendChild().then(callback)
+            }
         }
         return self
     }
     
-    @discardableResult func error(_ callback: @escaping ErrorBlock) -> Future<T> {
+    @discardableResult public func error(_ callback: @escaping ErrorBlock) -> Future<T> {
         operationQueue.addOperation {
             if let error = self.error { //If the future has already been rejected with an error. Call the block immediately.
                 callback(error)
             }
             
-            self.errorBlock = callback
+            if self.errorBlock == nil {
+                self.errorBlock = callback
+            }
+            else {
+                self.appendChild().error(callback)
+            }
         }
         return self
     }
+    
+    //MARK: - PRIVATE
     
     fileprivate func resolve(_ val: T) {
         operationQueue.addOperation {
@@ -77,6 +90,7 @@ class Future<T> {
             self.value = val
             
             self.thenBlock?(val)
+            self.childFuture?.resolve(val)
         }
     }
     
@@ -89,6 +103,16 @@ class Future<T> {
             self.error = err
             
             self.errorBlock?(err)
+            self.childFuture?.reject(err)
         }
+    }
+    
+    private func appendChild() -> Future<T> {
+        if let child = childFuture {
+            return child.appendChild()
+        }
+        let future = Future<T>()
+        childFuture = future
+        return future
     }
 }
